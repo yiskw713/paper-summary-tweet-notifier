25f0fa3217d4f1336765e8e0b61b98242e913a64
Yuchi Ishikawa <i.yuchi713@gmail.com>
2021-10-28 00:06:08 +0900

    Bug fix

--- src/libs/notify.py
+++ src/libs/notify.py
@@ -9,7 +9,7 @@ LINE_NOTIFY_API = "https://notify-api.line.me/api/notify"
 def notify_in_slack(slack_incoming_webhook_url: str, text_list: List[str]) -> None:
     slack_bot = slackweb.Slack(url=slack_incoming_webhook_url)
     for text in text_list:
-        slack_bot.notify(text)
+        slack_bot.notify(text=text)
 
 
 def notify_in_line(line_token: str, text_list: List[str]) -> None:
